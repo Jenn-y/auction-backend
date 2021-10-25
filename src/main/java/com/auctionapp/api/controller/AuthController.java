f54cae0fbbc9149df847feb6d97024f8f2c82a5b
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-10-25 23:41:40 +0200

    Added registration endoint in authentication controller

---
+++ src/main/java/com/auctionapp/api/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.auctionapp.api.controller;
+
+import com.auctionapp.api.model.dto.RegisterRequest;
+import com.auctionapp.api.service.AuthService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+  private final AuthService authService;
+  
+  @PostMapping("/register")
+  public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest){
+    authService.register(registerRequest);
+    return new ResponseEntity<>("User registration successful!", HttpStatus.OK);
+  }
+}
