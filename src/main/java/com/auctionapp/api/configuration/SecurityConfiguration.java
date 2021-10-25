644ce5e271a0492ae832baa79e3491ae71d4c877
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-10-25 23:40:03 +0200

    Added spring security configuration

---
+++ src/main/java/com/auctionapp/api/configuration/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.auctionapp.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  public void configure(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+        // httpSecurity.addFilterBefore(jwtAuthenticationFilter,
+        //         UsernamePasswordAuthenticationFilter.class);
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+  }
+}
