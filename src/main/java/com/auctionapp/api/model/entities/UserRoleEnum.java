97aac78d2d30c8d01a6c1fc29e3d881f55fe05f5
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-10-28 06:35:09 +0200

    Added findByEmail in user repository and user role enum

---
+++ src/main/java/com/auctionapp/api/model/entities/UserRoleEnum.java
@@ -0,0 +1,6 @@
+package com.auctionapp.api.model.entities;
+
+public enum UserRoleEnum {
+  USER,
+  ADMIN
+}
--- src/main/java/com/auctionapp/api/repository/UserRepository.java
+++ src/main/java/com/auctionapp/api/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.auctionapp.api.repository;
 
+import java.util.Optional;
+
 import com.auctionapp.api.model.entities.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-  
+
+  Optional<User> findByEmail(String email);
 }
