7fc463ac7e2e4312fc20abaa238f5b69218b4b3b
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-10-28 06:23:57 +0200

    Added login request and response models

---
+++ src/main/java/com/auctionapp/api/model/dto/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.auctionapp.api.model.dto;
+
+import com.auctionapp.api.model.entities.UserRoleEnum;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+  private String authenticationToken;
+  private String email;
+  private UserRoleEnum userRole;
+}
