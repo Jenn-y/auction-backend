309873b9f05d0b3eb05e31b9a822c1d309db61fb
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-10-28 15:53:40 +0200

    Added user details service implementation

--- src/main/java/com/auctionapp/api/service/UserDetailsServiceImpl.java
+++ src/main/java/com/auctionapp/api/service/UserDetailsServiceImpl.java
@@ -1,26 +1,26 @@
 package com.auctionapp.api.service;
 
-import static java.util.Collections.singletonList;
-
-import java.util.Collection;
-import java.util.Optional;
-
-import com.auctionapp.api.exceptions.UserNotFoundException;
-import com.auctionapp.api.model.entities.User;
-import com.auctionapp.api.repository.UserRepository;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
 
+import static java.util.Collections.singletonList;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import com.auctionapp.api.model.entities.User;
+import com.auctionapp.api.repository.UserRepository;
+
 @Service
 @AllArgsConstructor
-public class UserService implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
@@ -29,7 +29,7 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) {
         Optional<User> userOptional = userRepository.findByEmail(email);
         User user = userOptional
-                .orElseThrow(() -> new UserNotFoundException("No user " +
+                .orElseThrow(() -> new UsernameNotFoundException("No user " +
                         "Found with email :" + email));
 
         return new org.springframework.security.
@@ -40,4 +40,5 @@ public class UserService implements UserDetailsService {
     private Collection<? extends GrantedAuthority> getAuthorities(String role) {
         return singletonList(new SimpleGrantedAuthority(role));
     }
+
 }
