07b68a84f280ee73bb298a8826f9eba133b3fadc
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-11-04 21:26:18 +0100

    Fixed UUID generation

---
+++ src/main/resources/db/migration/V1__create_table_users.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS "users" (
+  "uuid" UUID DEFAULT gen_random_uuid () PRIMARY KEY,
+  "first_name" varchar(20),
+  "last_name" varchar(20),
+  "username" varchar(20),
+  "email" varchar(50) NOT NULL,
+  "password" varchar(255) NOT NULL,
+  "created_at" timestamp,
+  "updated_at" timestamp,
+  "role" varchar(255),
+  CONSTRAINT "email_unique" UNIQUE ("email")
+);
