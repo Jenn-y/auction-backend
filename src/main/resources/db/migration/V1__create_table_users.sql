6bd94d09994d9683816287943692d3a9192ab5e1
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-11-10 18:54:19 +0100

    Modified user migration

--- src/main/resources/db/migration/V1__create_table_users.sql
+++ src/main/resources/db/migration/V1__create_table_users.sql
@@ -1,14 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE TABLE IF NOT EXISTS "users" (
-  "id" UUID DEFAULT gen_random_uuid () PRIMARY KEY,
-  "first_name" varchar(20),
-  "last_name" varchar(20),
-  "username" varchar(20),
-  "email" varchar(50) NOT NULL,
-  "password" varchar(255) NOT NULL,
-  "created_at" timestamp,
-  "updated_at" timestamp,
-  "role" varchar(255),
-  CONSTRAINT "email_unique" UNIQUE ("email")
-);
+BEGIN
+  CREATE TABLE IF NOT EXISTS user_account (
+    id UUID DEFAULT gen_random_uuid () PRIMARY KEY,
+    first_name text,
+    last_name text,
+    username text,
+    email text NOT NULL,
+    password text NOT NULL,
+    created_at timestamp,
+    updated_at timestamp,
+    role text,
+    CONSTRAINT email_unique UNIQUE (email)
+  );
+COMMIT;
