aa9e15278a6e9279579efdac53b2c76eb849381b
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-11-11 15:19:58 +0100

    Modified migration for auctions, categories and items

--- src/main/resources/db/migration/V2__create_items_cat_auctions.sql
+++ src/main/resources/db/migration/V2__create_items_cat_auctions.sql
@@ -1,35 +1,35 @@
-CREATE TABLE IF NOT EXISTS "categories" (
-  "uuid" UUID DEFAULT gen_random_uuid () PRIMARY KEY,
-  "name" varchar(20) NOT NULL,
-  "subcategory" UUID,
+BEGIN;
+CREATE TABLE IF NOT EXISTS category (
+  id UUID DEFAULT gen_random_uuid () PRIMARY KEY,
+  name text NOT NULL,
+  subcategory UUID,
   CONSTRAINT fk_subcategory
       FOREIGN KEY(subcategory) 
-	  REFERENCES categories(uuid)
+	  REFERENCES category(id)
 );
 
-CREATE TABLE IF NOT EXISTS "items" (
-  "uuid" UUID DEFAULT gen_random_uuid () PRIMARY KEY,
-  "item_number" varchar(20) NOT NULL,
-  "name" varchar(50) NOT NULL,
-  "start_price" decimal,
-  "color" varchar(30),
-  "size" numeric,
-  "description" varchar(255)
+CREATE TABLE IF NOT EXISTS item (
+  id UUID DEFAULT gen_random_uuid () PRIMARY KEY,
+  name text NOT NULL,
+  startPrice decimal,
+  color text,
+  size numeric,
+  description text NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "auctions" (
-  "uuid" UUID DEFAULT gen_random_uuid () PRIMARY KEY,
-  "start_date" timestamp,
-  "end_date" timestamp,
-  "highest_bid" decimal,
-  "adress" varchar(50),
-  "zip_code" numeric,
-  "phone" varchar(50),
-  "status" varchar(255),
-  "shipping_cost_included" varchar(255),
-  "item" UUID,
-  "seller" UUID,
-  "category" UUID,
+CREATE TABLE IF NOT EXISTS auction (
+  id UUID DEFAULT gen_random_uuid () PRIMARY KEY,
+  startDate timestamp,
+  endDate timestamp,
+  highestBid decimal,
+  address text,
+  zipCode numeric,
+  phone text,
+  status text,
+  shippingCostIncluded text,
+  item UUID,
+  seller UUID,
+  category UUID,
   CONSTRAINT fk_category
       FOREIGN KEY(category) 
 	  REFERENCES categories(uuid),
@@ -40,3 +40,4 @@ CREATE TABLE IF NOT EXISTS "auctions" (
       FOREIGN KEY(seller) 
 	  REFERENCES users(uuid)
 );
+COMMIT;
