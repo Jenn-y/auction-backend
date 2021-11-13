6c014dc359f33ae6b0f7abb9c1e38b8483f86661
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-11-13 23:43:56 +0100

    Added bid controller with endpoint to get all bids by auction id

---
+++ src/main/java/com/auctionapp/api/controller/BidController.java
@@ -0,0 +1,32 @@
+package com.auctionapp.api.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.auctionapp.api.model.dto.BidDto;
+import com.auctionapp.api.service.BidService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bids")
+public class BidController {
+	
+	private final BidService service;
+
+	public BidController(final BidService bidService) {
+		this.service = bidService;
+	}
+
+	@GetMapping("/{auctionId}")
+	public ResponseEntity<List<BidDto>> getAuctionBids(@PathVariable final UUID auctionId) {
+		final List<BidDto> bids = service.getAuctionBids(auctionId);
+
+		return ResponseEntity.status(HttpStatus.OK).body(bids);
+	}
+}
