6cc5dacbf22d2fd8d97456121ccc8d6b19b350ba
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-12-24 23:32:26 +0100

    Swagger update

--- src/main/java/com/auctionapp/api/configuration/SwaggerConfiguration.java
+++ src/main/java/com/auctionapp/api/configuration/SwaggerConfiguration.java
@@ -18,7 +18,7 @@ public class SwaggerConfiguration {
     @Bean
     public Docket auction() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .host("https://bidba.herokuapp.com")
+                .host("https://bidba-api.herokuapp.com")
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
